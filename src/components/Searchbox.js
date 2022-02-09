cce03d844f2638957ad5a44cf64f5dba9c63a6ae
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-02-09 20:04:52 +0330

    try to add radium

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
-import styles from "./Searchbox.module.css"
+import React, { Component } from "react";
+import Radium from "radium";
+import styles from "./Searchbox.module.css";
 
 class Searchbox extends Component {
-    render() {
-        return (
-            <div className={styles.container}>
-                <p>Search What You Want :)</p>
-                <input  className={styles.search} type="search" placeholder='Type to search...'/>
-            </div>
-        );
-    }
+  myCss = {
+    color: "white",
+  };
+  render() {
+    return (
+      <div className={styles.container}>
+        <p style={this.myCss}>Search What You Want :)</p>
+        <input
+          className={styles.search}
+          type="search"
+          placeholder="Type to search..."
+        />
+      </div>
+    );
+  }
 }
 
 export default Searchbox;
