c510f97da9f6d9057236b1f54cf1bf46e5833802
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-02-12 12:31:17 +0330

    add radium

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import Cards from "./components/Cards.js";
 import Searchbox from "./components/Searchbox";
 import Logos from "./components/Logos.js";
 import Footer from "./components/Footer.js"
+import {StyleRoot} from "radium";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Banner />
-        <Cards />
-        <Searchbox />
-        <Logos />
-        <Footer/>
-      </header>
-    </div>
+    <StyleRoot>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Banner />
+          <Cards />
+          <Searchbox />
+          <Logos />
+          <Footer />
+        </header>
+      </div>
+    </StyleRoot>
   );
 }
 
--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Radium from "radium";
 import styles from "./Searchbox.module.css";
-const Searchbox = () => {
+
+class Searchbox extends Component {
   myCss = {
     color: "white",
-  };
-  return (
-    <div className={styles.container}>
-      <p style={this.myCss}>Search What You Want :)</p>
-      <input
-        className={styles.search}
-        type="search"
-        placeholder="Type to search..."
-      />
-    </div>
-  );
-};
+    ":hover":{
+      color: "red",
+      fontWeight:"lighter"
+    },
+    "@media(max-width:900px)":{
+      color:"black",
+      backgroundColor:"orange"
+    }
 
-export default Searchbox;
+  };
+  render() {
+    return (
+      <div className={styles.container}>
+        <p style={this.myCss}>Search What You Want :)</p>
+        <input
+          className={styles.search}
+          type="search"
+          placeholder="Type to search..."
+        />
+      </div>
+    );
+  }
+}
+
+export default Radium(Searchbox);
