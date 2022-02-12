6fd9f714b9ec010e27c3dad5b7de0db63dd793b2
mita4824 <87270333+mita4824@users.noreply.github.com>
2022-02-12 10:02:01 +0330

    add feature

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import Radium from "radium";
 import styles from "./Searchbox.module.css";
-
-class Searchbox extends Component {
+const Searchbox = () => {
   myCss = {
     color: "white",
   };
-  render() {
-    return (
-      <div className={styles.container}>
-        <p style={this.myCss}>Search What You Want :)</p>
-        <input
-          className={styles.search}
-          type="search"
-          placeholder="Type to search..."
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.container}>
+      <p style={this.myCss}>Search What You Want :)</p>
+      <input
+        className={styles.search}
+        type="search"
+        placeholder="Type to search..."
+      />
+    </div>
+  );
+};
 
 export default Searchbox;
